05907df646e6311f6530dbe2c4681e1c616d6e87
Tom Herold <tom.herold@scalableminds.com>
2016-04-11 21:36:59 +0200

    render motion tracker test as image grid

--- video_analysis/motion_tracking.py
+++ video_analysis/motion_tracking.py
@@ -1,14 +1,25 @@
 import numpy as np
 import cv2
 import sys
+import math
 
 if __name__ == '__main__':
 
-    videoFile = "sample/Himmelsdorf_480_cut.mp4"
+    videoFile = "snapshots/snap_%d.png"
     cap = cv2.VideoCapture(videoFile)
 
     if not cap.isOpened():
         sys.exit(1)
+
+
+    frame_counter = 0
+    num_columns = 5
+    fps = 100
+    grid_width = 1280
+    grid_height = 720
+
+    image_grid = np.zeros((grid_height, grid_width, 3), np.uint8)
+    image_grid_mask = np.zeros((grid_height, grid_width), np.uint8)
 
     while True:
 
@@ -33,49 +44,36 @@ if __name__ == '__main__':
         # only proceed if at least one contour was found
         if len(cnts) > 0:
             for cnt in cnts:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            #c = max(cnt, key=cv2.contourArea)
-                # ((x, y), radius) = cv2.minEnclosingCircle(cnt)
-                # M = cv2.moments(cnt)
-                # center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-                # # only proceed if the radius meets a minimum size
-                # #if radius > 10:
-                #     # draw the circle and centroid on the frame,
-                #     # then update the list of tracked points
-                # cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                # cv2.circle(frame, center, 5, (0, 0, 255), -1)
                 rect = cv2.boundingRect(cnt)
 
-
-                cv2.putText(frame, "%s, %s" % (rect[2], rect[3]), (rect[0], rect[1] + 5), 1, 1, (0, 255, 255))
+                #cv2.putText(frame, "%s, %s" % (rect[2], rect[3]), (rect[0], rect[1] + 5), 1, 1, (0, 255, 255))
                 if float(rect[2] / rect[3] + 0.001) > 3.5:
-                    cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]) , (0,0, 255), 2)
+                    cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]) , (0, 255, 255), 2)
 
-                    # val = cv2.fitEllipse(cnt)
-                    # cv2.ellipse(frame, val, (0, 0, 255), 4)
+        # render frame into a lager image grid
+        width = int(grid_width / float(num_columns))
+        height = int(grid_height / float(grid_width) * width)
+        column = (frame_counter % num_columns) * width
+        row = frame_counter / num_columns * height
 
+        image_grid[row:row + height, column:column + width] = cv2.resize(frame, (width, height))
+        image_grid_mask[row:row + height, column:column + width] = cv2.resize(mask, (width, height))
 
-        circleMask = cv2.circle(np.zeros_like(frame), (91, 394), 70, ((255, 255, 255)), -1) # dpad
-        circleMask = cv2.circle(circleMask, (83, 260), 43, ((255, 255, 255)), -1) # left shoot button
-
-        circleMask = cv2.cvtColor(circleMask, cv2.COLOR_BGR2GRAY)
-        # dst = cv2.inpaint(frame, circleMask, 5, cv2.INPAINT_TELEA)
-        # cv2.blur(frame, 5)
-
-        cv2.imshow("mask", mask)
-        cv2.imshow("orignal", frame)
-        cv2.moveWindow("orignal", 650, 50)
-
-        fps = 100
-        key = cv2.waitKey(1000 / fps) # time to wait between frames, in mSec
-        print key
+        # time to wait between frames, in mSec
+        # relevant for videos
+        key = cv2.waitKey(1000 / fps)
 
         # stop on ESC
         if key == 27: break
-        if key == 32: cv2.waitKey(0 )
+        if key == 32: cv2.waitKey(0)
+
+        frame_counter += 1
+
+
+    cv2.imshow("mask", image_grid_mask)
+    cv2.imshow("orignal", image_grid)
+    cv2.moveWindow("orignal", 650, 50)
+    cv2.waitKey(0)
 
     cap.release()
     cv2.destroyAllWindows()
